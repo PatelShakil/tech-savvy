8b0754ee0acd66168ceb101edae465413e4f8e52
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-27 13:44:36 +0530

    Logo Added

--- src/components/utils/RotatingLogo.tsx
+++ src/components/utils/RotatingLogo.tsx
@@ -3,7 +3,7 @@ import {motion} from "framer-motion";
 export const RotatingLogo = () => {
     return (
         <motion.img
-            src="/assets/logo.svg"
+            src="/assets/logo_gr_tr.png"
             alt="Logo"
             className="h-12 mr-2 rounded-full"
             animate={{ rotate: 360 }}
