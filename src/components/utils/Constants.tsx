a9a672a2f33c94897b28604731a1d1e9c9f2e68e
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-05-29 23:30:29 +0530

    Services Page Improved

--- src/components/utils/Constants.tsx
+++ src/components/utils/Constants.tsx
@@ -3,57 +3,85 @@ export const services: ServicesItem[] = [
     {
         id: 1,
         name: 'Coaching Classes',
-        color: 'green-500',
+        color: '#ec4343',
         icon: null,
         description: 'We offer a diverse range of classes designed to boost your skills and knowledge in various fields. Our experienced instructors provide hands-on training and in-depth insights, ensuring a comprehensive learning experience.'
     },
     {
         id: 2,
         name: 'Web Development',
-        color: 'yellow-400',
+        color: '#ed9c1f',
         icon: null,
         description: 'Our web development services focus on creating dynamic, responsive, and user-friendly websites. We specialize in the latest web technologies to ensure your online presence is robust and scalable.'
     },
     {
         id: 3,
         name: 'Android Development',
-        color: 'green-500',
+        color: '#88cc2e',
         icon: null,
         description: 'We build high-performance Android applications tailored to your needs. Our apps are designed for optimal user experience, leveraging the full potential of the Android platform to deliver exceptional functionality.'
     },
     {
         id: 4,
         name: 'College Projects',
-        color: 'purple-500',
+        color: '#3fc563',
         icon: null,
         description: 'Our team assists students in developing comprehensive college projects that meet academic standards. We provide guidance and technical support, ensuring your project is well-researched and professionally presented.'
     },
     {
         id: 5,
         name: 'Mobile Development',
-        color: 'gray-400',
+        color: '#39b8a6',
         icon: null,
         description: 'We create innovative mobile applications for various platforms, ensuring seamless functionality and superior user experience. Our solutions are tailored to meet your specific business needs and drive engagement.'
     },
     {
         id: 6,
         name: 'API Development',
-        color: 'indigo-500',
+        color: '#39a4e5',
         icon: null,
         description: 'Our API development services focus on building robust and secure APIs that enable seamless communication between different systems. We ensure our APIs are scalable, reliable, and easy to integrate.'
     },
     {
         id: 7,
         name: 'Backend Development',
-        color: 'purple-500',
+        color: '#8b56f1',
         icon: null,
         description: 'Specializing in server-side development, we ensure your application runs efficiently and securely. Our backend solutions are designed to support your front-end applications and provide a solid foundation for your digital presence.'
     },
     {
         id: 8,
         name: 'Desktop Development',
-        color: 'green-500',
+        color: '#d23aea',
         icon: null,
         description: 'We develop reliable and user-friendly desktop applications tailored to your specific needs. Our solutions are designed to enhance productivity and provide a seamless user experience across different operating systems.'
     }
 ];
+
+
+export function LightenDarkenColor(col: string, amt: number) {
+    let usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    const num = parseInt(col, 16);
+
+    let r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    let b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    let g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
--- src/components/utils/ServicesCard.tsx
+++ src/components/utils/ServicesCard.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import ServicesItem from "../../obj/ServicesItem.tsx";
+import {motion} from "framer-motion";
+import {LightenDarkenColor} from "./Constants.tsx";
 
 
 const ServiceCard: React.FC<ServicesItem> = ({ name, color, icon, description }) => {
 
     return (
-        <div className="w-full my-5 sm:mb-0 sm:w-1/2">
-            <div className={`relative h-full ml-0 mr-0 sm:mr-10 rounded-lg`}
+        <div className="w-full my-5 sm:mb-0 sm:w-1/2 bg-transparent">
+
+            <div
+                className={`relative h-full ml-0 mr-0 sm:mr-10 rounded-lg`}
             >
-                <span className={`absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-${color} rounded-lg`}></span>
-                <div className={`relative h-full p-5 bg-white border-2 hover:mt-0.5 hover:ml-0.5 border-${color} rounded-lg`}>
+                <span className={`absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg`}
+                      style={{
+                          backgroundColor: color
+                      }}
+                ></span>
+                <motion.div className={`relative h-full p-5 bg-white border-2  rounded-lg hover:text-white`}
+                     style={{
+                         borderColor: color
+                     }}
+
+                            whileHover={{
+                                scaleY:1.05,
+                                backgroundColor:LightenDarkenColor(color,10)
+                            }}
+                >
                     <div className="flex items-center -mt-1">
                         {icon && <img src={icon} alt={`${name} icon`} className="w-6 h-6"/>}
-                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">{name}</h3>
+                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800 hover:text-white">{name}</h3>
                     </div>
-                    <p className={`mt-2 mb-1 text-xs font-medium text-${color} uppercase`}>------------</p>
-                    <p className="mb-2 text-gray-600">{description}</p>
-                </div>
+                    <p className={`mt-2 mb-1 text-xs font-medium uppercase hover:text-white`}
+                       style={{
+                           color: color
+                       }}
+                    >------------</p>
+                    <p className="mb-2 text-gray-600 hover:text-white">{description}</p>
+                </motion.div>
             </div>
         </div>
     );
