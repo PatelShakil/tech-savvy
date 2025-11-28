4e16dcbca914e87451dfff5e002555c55fd26441
MuhammadShakil <patelsakib95@gmail.com>
2025-11-28 12:06:56 +0530

    Add PWA configuration with `vite-plugin-pwa` for enhanced offline support and app manifest inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["icon-96x96.png", "robots.txt", "icon-512x512.png"],
+            workbox: {
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB (or more)
+            },
+            manifest: {
+                name: "Tech Savvy Solution",
+                short_name: "TechSavvySolution",
+
+                theme_color: "#10162b",
+                background_color: "#000000",
+                display: "standalone",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
+    ]
+});
